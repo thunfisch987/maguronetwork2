3584cd9ef938f35c98d8570ecea06709fe57cdc7
codefactor-io <support@codefactor.io>
2023-04-23 20:25:38 +0000

    [CodeFactor] Apply fixes to commit 2518a3b
    [ci skip] [skip ci]

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
-	<ul style="color: white">
-		<li style="color: white" v-for="route in $router.getRoutes()">
-			{{ route.name?.toString().split("-")[0] }}
-		</li>
-	</ul>
+  <ul style="color: white">
+    <li
+      v-for="route in $router.getRoutes()"
+      style="color: white"
+    >
+      {{ route.name?.toString().split("-")[0] }}
+    </li>
+  </ul>
 </template>
