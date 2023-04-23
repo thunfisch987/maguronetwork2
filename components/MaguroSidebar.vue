83f149fed78fcea4b16f6198b62cca7a0704f668
codefactor-io <support@codefactor.io>
2023-04-23 13:54:22 +0000

    [CodeFactor] Apply fixes to commit a526cdf
    [ci skip] [skip ci]

--- components/MaguroSidebar.vue
+++ components/MaguroSidebar.vue
@@ -1,53 +1,65 @@
 <template>
-	<VaSidebar
-		position="left"
-		v-model="sidebarEnabled"
-		style="position: absolute; z-index: 3"
-	>
-		<VaSidebarItem @click="toggleSidebar(false)">
-			<VaSidebarItemContent style="text-align: center">
-				<VaSidebarItemTitle></VaSidebarItemTitle>
-				<VaButton icon="va-arrow-left"></VaButton>
-			</VaSidebarItemContent>
-		</VaSidebarItem>
-		<template
-			v-for="[itemname, item] of Object.entries(sidebarItems)"
-			:key="itemname"
-		>
-			<VaSidebarItem
-				v-if="!item.href"
-				:active="item.active"
-				:to="{ name: item.to }"
-			>
-				<VaSidebarItemContent>
-					<NuxtImg
-						v-if="itemname === 'RandomMemes'"
-						:src="item.image"
-						style="width: 2em; height: 2em"
-					/>
-					<NuxtImg
-						v-else-if="itemname === 'Nölz\' Weebsite'"
-						:src="weebsiteImages[weebsiteImages.current]"
-						style="width: 2em; height: 2em"
-						loading="lazy"
-					/>
-					<Icon v-else-if="item.icon" :name="item.icon" size="2em" />
-					<VaSidebarItemTitle>
-						{{ itemname }}
-					</VaSidebarItemTitle>
-				</VaSidebarItemContent>
-			</VaSidebarItem>
-			<VaSidebarItem v-else :active="item.active" :href="item.href">
-				<VaSidebarItemContent>
-					<Icon v-if="item.icon" :name="item.icon" size="2em" />
-					<VaSidebarItemTitle>
-						{{ itemname }}
-					</VaSidebarItemTitle>
-					<VaIcon name="open_in_new" />
-				</VaSidebarItemContent>
-			</VaSidebarItem>
-		</template>
-	</VaSidebar>
+  <VaSidebar
+    v-model="sidebarEnabled"
+    position="left"
+    style="position: absolute; z-index: 3"
+  >
+    <VaSidebarItem @click="toggleSidebar(false)">
+      <VaSidebarItemContent style="text-align: center">
+        <VaSidebarItemTitle />
+        <VaButton icon="va-arrow-left" />
+      </VaSidebarItemContent>
+    </VaSidebarItem>
+    <template
+      v-for="[itemname, item] of Object.entries(sidebarItems)"
+      :key="itemname"
+    >
+      <VaSidebarItem
+        v-if="!item.href"
+        :active="item.active"
+        :to="{ name: item.to }"
+      >
+        <VaSidebarItemContent>
+          <NuxtImg
+            v-if="itemname === 'RandomMemes'"
+            :src="item.image"
+            style="width: 2em; height: 2em"
+          />
+          <NuxtImg
+            v-else-if="itemname === 'Nölz\' Weebsite'"
+            :src="weebsiteImages[weebsiteImages.current]"
+            style="width: 2em; height: 2em"
+            loading="lazy"
+          />
+          <Icon
+            v-else-if="item.icon"
+            :name="item.icon"
+            size="2em"
+          />
+          <VaSidebarItemTitle>
+            {{ itemname }}
+          </VaSidebarItemTitle>
+        </VaSidebarItemContent>
+      </VaSidebarItem>
+      <VaSidebarItem
+        v-else
+        :active="item.active"
+        :href="item.href"
+      >
+        <VaSidebarItemContent>
+          <Icon
+            v-if="item.icon"
+            :name="item.icon"
+            size="2em"
+          />
+          <VaSidebarItemTitle>
+            {{ itemname }}
+          </VaSidebarItemTitle>
+          <VaIcon name="open_in_new" />
+        </VaSidebarItemContent>
+      </VaSidebarItem>
+    </template>
+  </VaSidebar>
 </template>
 
 <script lang="ts" setup>
